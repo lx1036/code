04994a15f1500104a8af222c7bf19c94a41d4e26
Xiang Liu <liuxiang3@360.cn>
2019-09-20 19:22:46 +0800

    (ts)radix tree

---
+++ typescript/algorithms/radix_tree/radix_tree.ts
@@ -0,0 +1,51 @@
+/**
+ * @see https://github.com/scofieldfan/turtle-rock/blob/ed74422c3775bcf99918246c4e2e4c175f9b8143/src/leetcode/trie.js#L1
+ */
+
+
+// @ts-ignore
+class Node {
+
+  set() {
+  
+  }
+  
+  get() {
+  
+  }
+  
+  public containsKey(key: string) {
+  
+  }
+}
+
+
+class Tree {
+  private root: Node;
+  
+  constructor() {
+    this.root = new Node();
+  }
+  
+  insert(word: string) {
+    for (let i = 0; i < word.length; i++) {
+    
+    }
+  }
+  
+  search(word: string) {
+  
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
