11f7a9cf4f7cc4bb82df241dc6cfb4e82a6002ed
Xiang Liu <lx1036@126.com>
2019-10-02 23:56:53 +0800

    (k8s wayne)message service

--- go/wayne/frontend/src/app/portal/app/app.component.ts
+++ go/wayne/frontend/src/app/portal/app/app.component.ts
@@ -1,5 +1,8 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, OnDestroy, OnInit} from '@angular/core';
 import {NamespaceClient} from "../../shared/client/kubernetes/namespace";
+import {CacheService} from "../../shared/auth/cache.service";
+import {DOCUMENT} from "@angular/common";
+import {MessageHandlerService} from "../../shared/message-handler/message-handler.service";
 
 const showState = {
   'name': {hidden: false},
@@ -8,6 +11,11 @@ const showState = {
   'create_user': {hidden: false},
   'action': {hidden: false}
 };
+
+interface ClusterCard {
+  name: string;
+  state: boolean;
+}
 
 @Component({
   selector: 'wayne-app',
@@ -23,13 +31,39 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   starredFilter: boolean;
   starredInherit: boolean; // starredInherit 用来传递给list
   
-  constructor(private namespaceClient: NamespaceClient, private cacheService: CacheService) { }
-
+  constructor(private namespaceClient: NamespaceClient,
+              private cacheService: CacheService,
+              @Inject(DOCUMENT) private document: any,
+              private element: ElementRef,
+              private messageHandlerService: MessageHandlerService) { }
+  
+  resources: object = {};
+  clusters: ClusterCard[] = [];
+  allowNumber = 10;
+  
   ngOnInit() {
     this.initShow();
-    this.starredFilter = (localStorage.getItem('starred') === 'true');
+    this.starredFilter = localStorage.getItem('starred') === 'true';
     this.starredInherit = this.starredFilter;
-    this.namespaceClient.getResourceUsage(this.cacheService.namespaceId).subscribe(response => {}, error => {});
+    this.namespaceClient.getResourceUsage(this.cacheService.namespaceId).subscribe(response => {
+      this.resources = response.data;
+      Object.getOwnPropertyNames(this.resources).forEach(cluster => {
+        this.clusters.push({name: cluster, state: false});
+      });
+  
+      this.allowNumber = this.getClusterMaxNumber();
+      for (let i = 0; i < this.allowNumber - 1; i++) {
+        setTimeout(((idx) => {
+          if (this.clusters[idx]) {
+            this.clusters[idx].state = true;
+          }
+        }).bind(this, i), 200 * i);
+      }
+    }, error => this.messageHandlerService.handleError(error));
+  }
+  
+  getClusterMaxNumber() {
+    return Math.floor(this.element.nativeElement.querySelector('.cluster-outline').offsetWidth / 255);
   }
   
   initShow() {
--- go/wayne/frontend/src/app/shared/shared.const.ts
+++ go/wayne/frontend/src/app/shared/shared.const.ts
@@ -2,3 +2,15 @@
 
 export const defaultRoutingUrl = 'portal/namespace/0/app';
 
+export const enum AlertType {
+  DANGER, WARNING, INFO, SUCCESS
+}
+
+export const httpStatusCode = {
+  'NoContent': 204,
+  'Unauthorized': 401,
+  'Forbidden': 403,
+  'NotFound': 404,
+  'StatusInternalServerError': 500
+};
+
