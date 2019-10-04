2b0c1e76479d16e84de75ff4f40b2f9bf4238b8b
Xiang Liu <lx1036@126.com>
2019-10-04 10:57:00 +0800

    (k8s wayne)app-user controller

--- go/wayne/backend/controllers/permission/app_user.go
+++ go/wayne/backend/controllers/permission/app_user.go
@@ -1,9 +1,20 @@
 package permission
 
-import "k8s-lx1036/wayne/backend/controllers/base"
+import (
+	"k8s-lx1036/wayne/backend/controllers/base"
+)
 
 type AppUserController struct {
 	base.APIController
+}
+
+func (c *AppUserController) URLMapping()  {
+	c.Mapping("List", c.List)
+	c.Mapping("Create", c.Create)
+	c.Mapping("Get", c.Get)
+	c.Mapping("GetPermissionByApp", c.GetPermissionByApp)
+	c.Mapping("Update", c.Update)
+	c.Mapping("Delete", c.Delete)
 }
 
 
