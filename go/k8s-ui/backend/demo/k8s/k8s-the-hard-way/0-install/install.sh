5b0d5620f84190988a46cfe80922460606f8746f
Xiang Liu <lx1036@126.com>
2020-01-31 23:39:41 +0800

    (k8s-the-hard-way)create gcp instances

---
+++ go/k8s-ui/backend/demo/k8s/k8s-the-hard-way/0-install/install.sh
@@ -0,0 +1,7 @@
+brew install cfssl
+cfssl version
+
+
+curl -o kubectl https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/darwin/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
--- go/k8s-ui/backend/demo/k8s/k8s-the-hard-way/k8s-controller-nodes.sh
+++
@@ -1,7 +0,0 @@
-for (( i = 0; i < 3; i++ )); do
-    gcloud compute instances create controller-${i} --async --boot-disk-size 200GB \
-      --can-ip-forward --image-family ubuntu-1804-lts \
-      --image-project ubuntu-os-cloud --machine-type n1-standard-1 --private-network-ip 10.240.0.1${i} \
-      --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
-      --subnet kubernetes --tags kubernetes-the-hard-way,controller
-done
