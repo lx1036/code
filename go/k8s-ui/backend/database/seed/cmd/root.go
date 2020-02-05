0a22375b5ba0aa6b3512381d5f66078792f02fd1
Xiang Liu <lx1036@126.com>
2020-02-06 01:05:21 +0800

    (k8s)add `seed` command in db

---
+++ go/k8s-ui/backend/database/seed/cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "db",
+		Short: "seed data to db",
+	}
+)
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.AddCommand(seedCmd)
+
+}
