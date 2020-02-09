fd1f6248de96fbd9e590774d225988d017b1aa3e
Xiang Liu <lx1036@126.com>
2020-02-10 02:28:50 +0800

    (k8s)add `Assert()` function

--- go/k8s-ui/backend/tests/controllers/auth_test.go
+++ go/k8s-ui/backend/tests/controllers/auth_test.go
@@ -9,10 +9,13 @@ import (
 	"github.com/stretchr/testify/suite"
 	"io/ioutil"
 	_ "k8s-lx1036/k8s-ui/backend/controllers/auth"
+	"k8s-lx1036/k8s-ui/backend/models"
 	routers_gin "k8s-lx1036/k8s-ui/backend/routers-gin"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
@@ -58,7 +61,6 @@ func (suite *AuthSuite) TestCors() {
 }
 
 func (suite *AuthSuite) TestLogin() {
-	//routers := routers_gin.SetupRouter()
 	body := struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
@@ -81,13 +83,11 @@ func (suite *AuthSuite) TestLogin() {
 	}
 
 	_ = json.Unmarshal(responseBody, &token)
-	//suite.Token = token.Data.Token
 
 	assert.Equal(suite.T(), http.StatusOK, response.StatusCode)
 }
 
 func (suite *AuthSuite) TestCurrentUser() {
-	//routers := routers_gin.SetupRouter()
 	request := httptest.NewRequest("GET", "/me", nil)
 	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", Token))
 	recorder := httptest.NewRecorder()
@@ -114,12 +114,40 @@ func (suite *AuthSuite) TestNotificationSubscribe() {
 	recorder := httptest.NewRecorder()
 	suite.routers.ServeHTTP(recorder, request)
 	response := recorder.Result()
-
-	// Assert(response)
-
+	defer response.Body.Close()
 	body, _ := ioutil.ReadAll(response.Body)
 
-	fmt.Println(string(body))
+	var notificationLogs struct{
+		Data []models.NotificationLog `json:"data"`
+	}
+
+	_ = json.Unmarshal(body, &notificationLogs)
+
+	var slices []interface{}
+	slices = append(slices, response.StatusCode)
+	slices = append(slices, response.Header)
+	slices = append(slices, notificationLogs.Data)
+
+	body2, _ := json.Marshal(slices)
+
+	path, err := filepath.Abs("./baseline/auth.json")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+
+		var buffer = new(bytes.Buffer)
+		err = json.Indent(buffer, body2, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		err = ioutil.WriteFile(path, buffer.Bytes(), 0666)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+
 }
 
 func (suite *AuthSuite) TestNotificationList() {
@@ -135,4 +163,88 @@ func (suite *AuthSuite) TestNotificationList() {
 
 func TestAuthSuite(test *testing.T) {
 	suite.Run(test, new(AuthSuite))
+}
+
+func getBaselineDataFile(function string) string  {
+	baselineDir := ""
+
+	if _, err := os.Stat(baselineDir); os.IsNotExist(err) {
+		err = os.MkdirAll(baselineDir, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return fmt.Sprintf("%s/%s.json", baselineDir, function)
+}
+
+func Assert(any interface{}) {
+	//fmt.Println("StatusCode", response.StatusCode)
+	//
+	//body2, _ := ioutil.ReadAll(response.Body)
+	//
+	//
+	header := http.Header{}
+	header.Add("Auth", "123")
+
+	//slices := make([]interface{})
+
+	var slices []interface{}
+	//slices[0] = struct {
+	//	Code int `json:"code"`
+	//}{
+	//	Code: 200,
+	//}
+	//slices[1] = struct {
+	//	Header http.Header `json:"header"`
+	//}{
+	//	Header: header,
+	//}
+
+
+	slices = append(slices, 200)
+	slices = append(slices, header)
+
+	var body2 struct{
+		Name string `json:"name"`
+	}
+	body2.Name = "lx1036"
+	slices = append(slices, body2)
+
+	//slices[2] = struct {
+	//	Content string `json:"content"`
+	//}{
+	//	Content: string(body2),
+	//}
+
+
+
+
+	//signature := fmt.Sprintf("%s:%s", )
+
+	//code := response.StatusCode
+	//
+	//var content struct{
+	//	Code int `json:"code"`
+	//	Header http.Header `json:"header"`
+	//
+	//}
+
+	body, _ := json.Marshal(slices)
+
+	path, err := filepath.Abs("./baseline/auth.json")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		var buffer = new(bytes.Buffer)
+		err = json.Indent(buffer, body, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		err = ioutil.WriteFile(path, buffer.Bytes(), 0666)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
