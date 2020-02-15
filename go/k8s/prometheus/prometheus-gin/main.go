1de65e75580cadfee188ea2421829dd431282362
Xiang Liu <lx1036@126.com>
2020-02-15 19:18:53 +0800

    (k8s)move files

--- go/k8s/prometheus/prometheus-gin/main.go
+++ go/k8s/prometheus/prometheus-gin/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
-	"k8s-lx1036/k8s/prometheus-gin/prometheus"
+	"k8s-lx1036/k8s/prometheus/prometheus-gin/prometheus"
 	"net/http"
 	"net/url"
 	"sort"
