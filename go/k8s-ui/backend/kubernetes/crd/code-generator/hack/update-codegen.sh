94614c5156b1350da56b8b366a86f95278823598
Xiang Liu <lx1036@126.com>
2020-02-25 01:48:59 +0800

    (k8s)code generator

---
+++ go/k8s-ui/backend/kubernetes/crd/code-generator/hack/update-codegen.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+## Project specific data
+#PROJECT_PACKAGE=github.com/slok/kube-code-generator/example
+#CLIENT_GENERATOR_OUT=${PROJECT_PACKAGE}/client
+#APIS_ROOT=${PROJECT_PACKAGE}/apis
+#
+## Ugly but needs to be relative if we want to use k8s.io/code-generator
+## as it is without touching/sed-ing the code/scripts
+#RELATIVE_ROOT_PATH=$(realpath --relative-to="${PWD}" /)
+#CODEGEN_PKG=${RELATIVE_ROOT_PATH}${GOPATH}/src/k8s.io/code-generator
+#
+## Add all groups space separated.
+## Example: GROUPS_VERSION="xxxx:v1alpha1 yyyy:v1"
+#GROUPS_VERSION="test:v1alpha1"
+#
+## Generation targets
+## Example:
+#GENERATION_TARGETS="deepcopy,client"
+
+
+[ -z "$PROJECT_PACKAGE" ] && echo "PROJECT_PACKAGE env var is required" && exit 1;
+[ -z "$CLIENT_GENERATOR_OUT" ] && echo "CLIENT_GENERATOR_OUT env var is required" && exit 1;
+[ -z "$APIS_ROOT" ] && echo "APIS_ROOT env var is required" && exit 1;
+[ -z "$GROUPS_VERSION" ] && echo "GROUPS_VERSION env var is required" && exit 1;
+
+
+GENERATION_TARGETS="${GENERATION_TARGETS:-all}"
+
+# Ugly but needs to be relative if we want to use k8s.io/code-generator
+# as it is without touching/sed-ing the code/scripts
+RELATIVE_ROOT_PATH=$(realpath --relative-to="${PWD}" /)
+CODEGEN_PKG=${RELATIVE_ROOT_PATH}${GOPATH}/src/k8s.io/code-generator
+
+# Only generate deepcopy (runtime object needs) and typed client.
+# Typed listers & informers not required for the moment. Used with generic
+# custom informer/listerwatchers.
+# https://blog.openshift.com/kubernetes-deep-dive-code-generation-customresources/
+
+
+
+
+${CODEGEN_PKG}/generate-groups.sh GENERATION_TARGETS \
+  ${CLIENT_GENERATOR_OUT} \
+  ${APIS_ROOT} \
+  "${GROUPS_VERSION}"
