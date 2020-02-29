8800b072cbdcb13d28853698fb04d6da89fa83ca
Xiang Liu <lx1036@126.com>
2020-03-01 00:55:58 +0800

    (k8s dashboard)list deployment

--- go/dashboard/backend/api/types.go
+++ go/dashboard/backend/api/types.go
@@ -4,3 +4,10 @@ package api
 // code that does things based on the kind. For example, there may be a generic "deleter"
 // that based on resource kind, name and namespace deletes it.
 type ResourceKind string
+
+
+// ListMeta describes list of objects, i.e. holds information about pagination options set for the list.
+type ListMeta struct {
+	// Total number of items on the list. Used for pagination.
+	TotalItems int `json:"totalItems"`
+}
