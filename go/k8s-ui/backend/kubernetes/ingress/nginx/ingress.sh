bf767c8567c0e3758c69cec45a541cc2b1246545
Xiang Liu <lx1036@126.com>
2020-04-26 23:30:05 +0800

    (ingress)Makefile

--- go/k8s-ui/backend/kubernetes/ingress/nginx/ingress.sh
+++
@@ -1,6 +0,0 @@
-
-curl "$(minikube service web --url)"
-
-kubectl exec -it \
-  "$(kubectl get pods -n kube-system -l app.kubernetes.io/name=nginx-ingress-controller -o=jsonpath='{.items[0].metadata.name}')" \
-  -n kube-system -- cat /etc/nginx/nginx.conf > ingress-nginx-raw.conf
