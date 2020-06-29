4aaa07b59ecc1bca1c058824c5d216ee86cdcea3
Xiang Liu <lx1036@126.com>
2020-06-29 19:32:39 +0800

    (k8s)nginx-ingress tls

--- go/k8s/network/ingress/nginx/auth/client-certs/secrets.sh
+++ go/k8s/network/ingress/nginx/auth/client-certs/secrets.sh
@@ -1,4 +1,9 @@
 
-cat ca.crt server.crt client.crt >> ca-2.crt
-kubectl create secret generic ca-secret --from-file=ca.crt=ca-2.crt
-kubectl create secret generic tls-secret --from-file=tls.crt=server.crt --from-file=tls.key=server.key
+#cat ca.crt server.crt client.crt >> ca-2.crt
+#kubectl create secret generic ca-secret --from-file=ca.crt=ca.crt
+#kubectl create secret generic tls-secret --from-file=tls.crt=server.crt --from-file=tls.key=server.key
+
+
+
+kubectl delete secret ca-secret
+kubectl create secret generic ca-secret --from-file=tls.crt=server.crt --from-file=tls.key=server.key --from-file=ca.crt=ca.crt
