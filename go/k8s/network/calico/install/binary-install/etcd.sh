1a5a5ffb7db1330b8deb7f4de6c64375065f81b6
Xiang Liu <lx1036@126.com>
2020-08-10 15:23:52 +0800

    (calico)calico-libnetwork-plugin service

--- go/k8s/network/calico/install/binary-install/etcd.sh
+++ go/k8s/network/calico/install/binary-install/etcd.sh
@@ -1,14 +1,45 @@
+# centos
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum -y install docker-ce
+sudo systemctl start docker
+sudo systemctl status docker
+echo '{"debug":true,"registry-mirrors":["https://wvtedxym.mirror.aliyuncs.com"],"experimental":true}' > /etc/docker/daemon.json
+sudo systemctl restart docker
+
 
 CLUSTER_TOKEN := "etcd-cluster-1"
 CLUSTER := "etcd2379=http://0.0.0.0:2380"
+
+
+rm -rf /tmp/etcd-data.tmp && mkdir -p /tmp/etcd-data.tmp && \
+  docker rmi quay.io/coreos/etcd:v3.4.9 || true && \
+  docker run \
+  -p 2379:2379 \
+  -p 2380:2380 \
+  --mount type=bind,source=/tmp/etcd-data.tmp,destination=/etcd-data \
+  --name etcd-v3.4.9 \
+  quay.io/coreos/etcd:v3.4.9 \
+  /usr/local/bin/etcd \
+  --name s1 \
+  --data-dir /etcd-data \
+  --listen-client-urls http://0.0.0.0:2379 \
+  --advertise-client-urls http://0.0.0.0:2379 \
+  --listen-peer-urls http://0.0.0.0:2380 \
+  --initial-advertise-peer-urls http://0.0.0.0:2380 \
+  --initial-cluster s1=http://0.0.0.0:2380 \
+  --initial-cluster-token tkn \
+  --initial-cluster-state new \
+  --logger zap \
+  --log-outputs stderr
+
 
 etcd --name etcd2379 \
       --listen-client-urls http://0.0.0.0:2379 \
       --advertise-client-urls http://0.0.0.0:2379 \
       --listen-peer-urls http://0.0.0.0:2380 \
       --initial-advertise-peer-urls http://0.0.0.0:2380 \
-      --initial-cluster-token $(CLUSTER_TOKEN) \
-      --initial-cluster $(CLUSTER) \
+      --initial-cluster-token "etcd-cluster-1" \
+      --initial-cluster etcd2379=http://0.0.0.0:2380 \
       --initial-cluster-state new \
       --enable-pprof \
       --logger=zap \
