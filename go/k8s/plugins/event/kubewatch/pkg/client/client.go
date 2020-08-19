42b63a7eb14a06736060298356e1214301e07929
Xiang Liu <lx1036@126.com>
2020-08-20 04:21:14 +0800

    (k8s)informer

--- go/k8s/plugins/event/kubewatch/pkg/client/client.go
+++ go/k8s/plugins/event/kubewatch/pkg/client/client.go
@@ -1,11 +1,31 @@
 package client
 
-import "k8s.io/client-go/kubernetes"
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	log "github.com/sirupsen/logrus"
+)
 
 var (
 	KubeClient kubernetes.Interface 
 )
 
-func GetKubeClient()  {
-	
+func GetKubeClient(kubeconfig string) kubernetes.Interface {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err.Error(),
+		}).Error("[kubeconfig]")
+		return nil
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err.Error(),
+		}).Error("[kubeclient]")
+		return nil
+	}
+
+	return client
 }
