9ee3ed06a92ad50bf3db99381d967ff534b21a1a
Xiang Liu <lx1036@126.com>
2020-08-30 01:21:30 +0800

    (go)select demo

---
+++ go/k8s/concepts/go/channel/select/channel.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	//demo1()
+	demo2()
+}
+
+func demo1() {
+	stop := make(chan os.Signal)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case <-time.Tick(time.Second * 2):
+		fmt.Println("hello")
+	case <-stop:
+		fmt.Println("world")
+	}
+
+	<-stop
+}
+
+func demo2() {
+	stop := make(chan os.Signal)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	opened := false
+	for  {
+		select {
+		case <-time.Tick(time.Second * 2):
+			fmt.Println("hello")
+		case <-stop:
+			fmt.Println("world")
+			close(stop)
+			opened = true
+		}
+
+		if opened {
+			break
+		}
+	}
+}
