fc04f86f9063bd26a708dbaef0c3abe3152f4cc9
Xiang Liu <lx1036@126.com>
2020-12-18 10:53:23 +0800

    (operator)NamespaceController, delete all resources in specified namespace

--- go/k8s/concepts/components/controller-manager/namespace-controller/pkg/controller/namespaced_resources_deleter.go
+++ go/k8s/concepts/components/controller-manager/namespace-controller/pkg/controller/namespaced_resources_deleter.go
@@ -2,21 +2,53 @@ package controller
 
 import (
 	"fmt"
+	"sync"
 
 	"k8s-lx1036/k8s/concepts/components/controller-manager/namespace-controller/pkg/debug"
 
 	log "github.com/sirupsen/logrus"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
+// operation is used for caching if an operation is supported on a dynamic client.
+type operation string
+
+const (
+	operationDeleteCollection operation = "deletecollection"
+	operationList             operation = "list"
+	// assume a default estimate for finalizers to complete when found on items pending deletion.
+	finalizerEstimateSeconds int64 = int64(15)
+)
+
+// operationKey is an entry in a cache.
+type operationKey struct {
+	operation operation
+	gvr       schema.GroupVersionResource
+}
+
+type operationNotSupportedCache struct {
+	lock sync.RWMutex
+	m    map[operationKey]bool
+}
+
+func (o *operationNotSupportedCache) setNotSupported(key operationKey) {
+	o.lock.Lock()
+	defer o.lock.Unlock()
+	o.m[key] = true
+}
+
 type NamespacedResourcesDeleterInterface interface {
-	Delete(namespace string) error
+	Delete(namespaceName string) error
 }
 
 type namespacedResourcesDeleter struct {
 	discoverResourcesFn func() ([]*metav1.APIResourceList, error)
+
+	opCache *operationNotSupportedCache
 }
 
 func NewNamespacedResourcesDeleter(
@@ -43,8 +75,46 @@ func (deleter *namespacedResourcesDeleter) initOpCache() {
 
 	debug.LogAPIResourceList(resources)
 
+	// 过滤出verb只有list和deletecollection的resource
+	var deletableGroupVersionResources []schema.GroupVersionResource
+	for _, resource := range resources {
+		gv, err := schema.ParseGroupVersion(resource.GroupVersion)
+		if err != nil {
+			log.Errorf("Failed to parse GroupVersion %q, skipping: %v", resource.GroupVersion, err)
+			continue
+		}
+
+		for _, apiResource := range resource.APIResources {
+			gvr := schema.GroupVersionResource{Group: gv.Group, Version: gv.Version, Resource: apiResource.Name}
+			verbs := sets.NewString([]string(apiResource.Verbs)...)
+
+			if !verbs.Has("delete") {
+				log.Infof("Skipping resource %v because it cannot be deleted.", gvr)
+			}
+
+			// verb没有list和deletecollection的resource，cache到opCache对象
+			for _, op := range []operation{operationList, operationDeleteCollection} {
+				if !verbs.Has(string(op)) {
+					deleter.opCache.setNotSupported(operationKey{operation: op, gvr: gvr})
+				}
+			}
+
+			deletableGroupVersionResources = append(deletableGroupVersionResources, gvr)
+		}
+	}
 }
 
-func (deleter *namespacedResourcesDeleter) Delete(namespace string) error {
+// 删除给定namespace中的所有资源对象
+// 删除前：检查namespace.DeletionTimestamp字段值；检查namespace.Status.Phase是不是"Terminating"状态
+// 删除后：移除namespace.Spec.Finalizers并finalize下namespace
+func (deleter *namespacedResourcesDeleter) Delete(namespaceName string) error {
 	return nil
+}
+
+type ResourcesRemainingError struct {
+	Estimate int64
+}
+
+func (e *ResourcesRemainingError) Error() string {
+	return fmt.Sprintf("some content remains in the namespace, estimate %d seconds before it is removed", e.Estimate)
 }
