bcabfdb0b6e40a42eb84b0f98751d03185d7c236
Xiang Liu <lx1036@126.com>
2021-03-24 16:56:08 +0800

    (scheduler)priority-class-fit plugin

--- go/k8s/scheduler/demo/extend-scheduler/pkg/plugins/priorityclass/priority_class.go
+++ go/k8s/scheduler/demo/extend-scheduler/pkg/plugins/priorityclass/priority_class.go
@@ -3,59 +3,107 @@ package priorityclass
 import (
 	"context"
 	"fmt"
+	"strconv"
+
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog/v2"
 	frameworkRuntime "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
 	"k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
 )
 
-const Name = "sample-plugin"
+const Name = "priority-class-fit"
+
+const (
+	CPUAllocateAnnotation    = "allocatable.resource/cpu"
+	MemoryAllocateAnnotation = "allocatable.resource/memory"
+)
 
 type Args struct {
-	FavoriteColor     string `json:"favorite_color,omitempty"`
-	FavoriteNumber    int    `json:"favorite_number,omitempty"`
-	ThanksTo          string `json:"thanks_to,omitempty"`
 	PriorityClassName string `json:"priorityClassName,omitempty"`
+	Ratio             int    `json:"ratio,omitempty"`
 }
 
-// @see k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1/interface.go
-type Sample struct {
+// @see https://github.com/kubernetes/kubernetes/blob/master/pkg/scheduler/framework/plugins/noderesources/fit.go
+type PriorityClassFit struct {
 	args   *Args
 	handle v1alpha1.FrameworkHandle
 }
 
-func (s *Sample) Name() string {
+func (s *PriorityClassFit) Name() string {
 	return Name
 }
 
-func (s *Sample) PreFilter(pc context.Context, state *v1alpha1.CycleState, pod *v1.Pod) *v1alpha1.Status {
-	klog.V(3).Infof("prefilter pod: %v", pod.Name)
-	return v1alpha1.NewStatus(v1alpha1.Success, "")
-}
-
-func (s *Sample) PreFilterExtensions() v1alpha1.PreFilterExtensions {
-	return nil
-}
-
-func (s *Sample) Filter(pc context.Context, state *v1alpha1.CycleState, pod *v1.Pod, nodeName *v1alpha1.NodeInfo) *v1alpha1.Status {
-	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeName.Node().Name)
-	return v1alpha1.NewStatus(v1alpha1.Success, "")
-}
-
-func (s *Sample) PreBind(pc context.Context, state *v1alpha1.CycleState, pod *v1.Pod, nodeName string) *v1alpha1.Status {
-	if nodeInfo, err := s.handle.SnapshotSharedLister().NodeInfos().Get(nodeName); err != nil {
-		return v1alpha1.NewStatus(v1alpha1.Error, fmt.Sprintf("prebind get node info error: %+v", nodeName))
-	} else {
-		klog.Infof("prebind node info: %+v", nodeInfo.Node().Name)
-		return v1alpha1.NewStatus(v1alpha1.Success, "")
+func (s *PriorityClassFit) Filter(c context.Context, state *v1alpha1.CycleState, pod *v1.Pod, nodeInfo *v1alpha1.NodeInfo) *v1alpha1.Status {
+	if pod.Spec.PriorityClassName != s.args.PriorityClassName {
+		return nil
 	}
+
+	// calculate sum * ratio cpu/memory
+	node := nodeInfo.Node()
+	cpuStr, ok := node.Annotations[CPUAllocateAnnotation] // e.g. "30"
+	if !ok {
+		return nil
+	}
+	cpu, err := strconv.ParseInt(cpuStr, 10, 0)
+	if err != nil {
+		klog.Errorf("parse int err %v", err)
+		return nil
+	}
+	cpu = cpu * 1000 * int64(s.args.Ratio)
+	sumCores := resource.MustParse(fmt.Sprintf("%dm", cpu))
+	klog.Infof("sum * ration: milliCores = %v (%v) in node %s", sumCores.MilliValue(), sumCores.Format, nodeInfo.Node().Name)
+	memoryStr, ok := node.Annotations[MemoryAllocateAnnotation] // e.g. "122991640Ki"
+	if !ok {
+		return nil
+	}
+	memory := resource.MustParse(memoryStr)
+	sumMemory := resource.NewQuantity(memory.Value()*int64(s.args.Ratio), resource.BinarySI)
+	klog.Infof("sum * ration: memorySize = %v (%v) in node %s", sumMemory.Value(), sumMemory.Format, nodeInfo.Node().Name)
+
+	consumedCores := resource.NewMilliQuantity(0, resource.DecimalSI)
+	consumedMemory := resource.NewQuantity(0, resource.BinarySI)
+	for _, item := range nodeInfo.Pods {
+		if item.Pod.Spec.NodeName == node.Name && item.Pod.Spec.PriorityClassName == s.args.PriorityClassName {
+			// calculate consumed cpu/memory
+			for _, container := range item.Pod.Spec.Containers {
+				for name, quantity := range container.Resources.Requests {
+					switch name {
+					case v1.ResourceCPU:
+						consumedCores.Add(quantity)
+					case v1.ResourceMemory:
+						consumedMemory.Add(quantity)
+					}
+				}
+			}
+		}
+	}
+
+	// add current pod
+	for _, container := range pod.Spec.Containers {
+		for name, quantity := range container.Resources.Requests {
+			switch name {
+			case v1.ResourceCPU:
+				consumedCores.Add(quantity)
+			case v1.ResourceMemory:
+				consumedMemory.Add(quantity)
+			}
+		}
+	}
+
+	if consumedCores.Cmp(sumCores) == -1 && consumedMemory.Cmp(*sumMemory) == -1 {
+		return nil
+	}
+
+	return v1alpha1.NewStatus(v1alpha1.Unschedulable, "cpu or memory resource is insufficient")
 }
 
 // 在 pkg/scheduler/framework/v1alpha1/interface.go 定义
-func (s *Sample) Score(ctx context.Context, state *v1alpha1.CycleState, pod *v1.Pod, nodeName string) (int64, *v1alpha1.Status) {
+func (s *PriorityClassFit) Score(ctx context.Context, state *v1alpha1.CycleState, pod *v1.Pod, nodeName string) (int64, *v1alpha1.Status) {
+	// 只考虑 "PriorityClassName" pod
 	if pod.Spec.PriorityClassName != s.args.PriorityClassName {
-		return 0, v1alpha1.NewStatus(v1alpha1.Success)
+		return 0, nil
 	}
 
 	nodeInfo, err := s.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
@@ -71,7 +119,7 @@ func (s *Sample) Score(ctx context.Context, state *v1alpha1.CycleState, pod *v1.
 		}
 	}
 
-	return int64(score), v1alpha1.NewStatus(v1alpha1.Success)
+	return int64(score), nil
 }
 
 func New(configuration runtime.Object, handle v1alpha1.FrameworkHandle) (v1alpha1.Plugin, error) {
@@ -81,11 +129,14 @@ func New(configuration runtime.Object, handle v1alpha1.FrameworkHandle) (v1alpha
 	}
 
 	if len(args.PriorityClassName) == 0 {
-		return nil, fmt.Errorf("")
+		args.PriorityClassName = "system-cluster-critical"
+	}
+	if args.Ratio == 0 {
+		args.Ratio = 1
 	}
 
-	klog.Infof("get plugin config args: %+v", args)
-	return &Sample{
+	klog.Infof("get plugin config args: %v", args)
+	return &PriorityClassFit{
 		args:   args,
 		handle: handle,
 	}, nil
