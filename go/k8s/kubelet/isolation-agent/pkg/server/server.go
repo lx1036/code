6a2de561fcb2fd7341634e87a1944768c1d83c95
Xiang Liu <lx1036@126.com>
2021-04-29 12:20:06 +0800

    (colocation)take cpu by topology scraped by cadvisor

--- go/k8s/kubelet/isolation-agent/pkg/server/server.go
+++ go/k8s/kubelet/isolation-agent/pkg/server/server.go
@@ -7,8 +7,9 @@ import (
 	"time"
 
 	"k8s-lx1036/k8s/kubelet/isolation-agent/pkg/cgroup"
-	"k8s-lx1036/k8s/kubelet/isolation-agent/pkg/policy"
 	"k8s-lx1036/k8s/kubelet/isolation-agent/pkg/scraper"
+	topologycpu "k8s-lx1036/k8s/kubelet/isolation-agent/pkg/topology"
+	"k8s-lx1036/k8s/kubelet/isolation-agent/pkg/utils"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,6 +24,8 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 	api "k8s.io/kubernetes/pkg/apis/core"
+	"k8s.io/kubernetes/pkg/kubelet/cadvisor"
+	"k8s.io/kubernetes/pkg/kubelet/cm/cpumanager/topology"
 	"k8s.io/kubernetes/pkg/kubelet/cm/cpuset"
 	kubecontainer "k8s.io/kubernetes/pkg/kubelet/container"
 	resourceclient "k8s.io/metrics/pkg/client/clientset/versioned/typed/metrics/v1beta1"
@@ -49,9 +52,12 @@ type Server struct {
 	informer  informers.SharedInformerFactory
 	podLister v1.PodLister
 
-	scraper       *scraper.Scraper
-	kubeClient    *kubernetes.Clientset
-	cgroupManager *cgroup.Manager
+	scraper               *scraper.Scraper
+	kubeClient            *kubernetes.Clientset
+	cgroupManager         *cgroup.Manager
+	capacity              corev1.ResourceList
+	cpuTopology           *topology.CPUTopology
+	remoteRuntimeEndpoint string
 }
 
 func NewRestConfig(kubeconfig string) (*rest.Config, error) {
@@ -90,16 +96,50 @@ func NewServer(config *Config) (*Server, error) {
 	cgroupManager := cgroup.NewManager(config.RemoteRuntimeEndpoint, config.ConnectionTimeout)
 
 	return &Server{
-		nodeName:      config.Nodename,
-		scraper:       scraper.NewScraper(metricsClient),
-		kubeClient:    kubeClient,
-		cgroupManager: cgroupManager,
-		sync:          informer.Core().V1().Pods().Informer().HasSynced,
-		informer:      informer,
-		podLister:     informer.Core().V1().Pods().Lister(),
-		resolution:    config.MetricResolution,
+		//cpuTopology: cpuTopology,
+		//capacity: capacity,
+		remoteRuntimeEndpoint: config.RemoteRuntimeEndpoint,
+		nodeName:              config.Nodename,
+		scraper:               scraper.NewScraper(metricsClient),
+		kubeClient:            kubeClient,
+		cgroupManager:         cgroupManager,
+		sync:                  informer.Core().V1().Pods().Informer().HasSynced,
+		informer:              informer,
+		podLister:             informer.Core().V1().Pods().Lister(),
+		resolution:            config.MetricResolution,
 	}, nil
+}
 
+func (server *Server) getCPUTopology() (*topology.CPUTopology, corev1.ResourceList) {
+	containerRuntime := "docker"
+	rootDirectory := "/var/lib/kubelet"
+	//remoteRuntimeEndpoint := "unix:///var/run/dockershim.sock"
+	cgroupRoots := []string{"/kubepods"}
+	imageFsInfoProvider := cadvisor.NewImageFsInfoProvider(containerRuntime, server.remoteRuntimeEndpoint)
+	cadvisorClient, err := cadvisor.New(imageFsInfoProvider, rootDirectory, cgroupRoots,
+		cadvisor.UsingLegacyCadvisorStats(containerRuntime, server.remoteRuntimeEndpoint))
+	if err != nil {
+		panic(err)
+	}
+	machineInfo, err := cadvisorClient.MachineInfo()
+	if err != nil {
+		panic(err)
+	}
+	capacity := cadvisor.CapacityFromMachineInfo(machineInfo)
+	klog.Info(fmt.Sprintf("cpu: %s, memory: %s", capacity.Cpu().String(), capacity.Memory().String()))
+	numaNodeInfo, err := topology.GetNUMANodeInfo()
+	if err != nil {
+		panic(err)
+	}
+	cpuTopology, err := topology.Discover(machineInfo, numaNodeInfo)
+	if err != nil {
+		panic(err)
+	}
+	allCPUs := cpuTopology.CPUDetails.CPUs()
+	klog.Info(fmt.Sprintf("NumCPUs[processor,逻辑核]: %d, NumCores[core,物理核]: %d, NumSockets[NUMA node]: %d, allCPUs: %s",
+		cpuTopology.NumCPUs, cpuTopology.NumCores, cpuTopology.NumSockets, allCPUs.String()))
+
+	return cpuTopology, capacity
 }
 
 func (server *Server) RunUntil(stopCh <-chan struct{}) error {
@@ -128,6 +168,10 @@ func (server *Server) RunUntil(stopCh <-chan struct{}) error {
 }
 
 func (server *Server) tick(ctx context.Context, startTime time.Time) {
+
+	// 每次都重新计算 cpu topology
+	cpuTopo, capacity := server.getCPUTopology()
+
 	pods, err := server.podLister.Pods(metav1.NamespaceAll).List(labels.Everything())
 	if err != nil {
 		klog.Error(fmt.Sprintf("get pods in node %s err: %v", server.nodeName, err))
@@ -139,24 +183,56 @@ func (server *Server) tick(ctx context.Context, startTime time.Time) {
 		klog.Error(err)
 	}
 
-	currentNode, err := server.kubeClient.CoreV1().Nodes().Get(context.TODO(), server.nodeName, metav1.GetOptions{})
+	/*
+		currentNode, err := server.kubeClient.CoreV1().Nodes().Get(context.TODO(), server.nodeName, metav1.GetOptions{})
+		if err != nil {
+			klog.Error(err)
+			return
+		}
+		// INFO: 由于会超卖，所以 Allocatable 不是 node 的资源实际值，可以考虑使用
+		//  machineInfo, err := cadvisorClient.MachineInfo()
+		//  capacity := cadvisor.CapacityFromMachineInfo(machineInfo), 这里 capacity 就是 currentNode.Status.Capacity
+		prodCpuRatio := float64(prodMetrics.Cpu().Value()) / float64(currentNode.Status.Allocatable.Cpu().Value())
+		nonProdCpuRatio := float64(nonProdMetrics.Cpu().Value()) / float64(currentNode.Status.Allocatable.Cpu().Value())
+		prodMemoryRatio := float64(prodMetrics.Memory().Value()) / float64(currentNode.Status.Allocatable.Memory().Value())
+		nonProdMemoryRatio := float64(nonProdMetrics.Memory().Value()) / float64(currentNode.Status.Allocatable.Memory().Value())
+	*/
+	prodCpuRatio := float64(prodMetrics.Cpu().Value()) / float64(capacity.Cpu().Value())
+	nonProdCpuRatio := float64(nonProdMetrics.Cpu().Value()) / float64(capacity.Cpu().Value())
+	prodMemoryRatio := float64(prodMetrics.Memory().Value()) / float64(capacity.Memory().Value())
+	nonProdMemoryRatio := float64(nonProdMetrics.Memory().Value()) / float64(capacity.Memory().Value())
+
+	klog.Info(prodCpuRatio, nonProdCpuRatio, prodMemoryRatio, nonProdMemoryRatio)
+
+	allCPUs := cpuTopo.CPUDetails.CPUs()
+	// TODO: policy, 先草稿下后面再继续完善 policy
+	numReservedCPUs := 2 // 最小值得是2，即至少一个物理核
+	if prodCpuRatio < 0.2 {
+		numReservedCPUs = numReservedCPUs * 2
+		if numReservedCPUs > allCPUs.Size() {
+			klog.Warning("")
+			numReservedCPUs = 2
+		}
+	}
+
+	reserved, err := topologycpu.TakeCPUByTopology(cpuTopo, allCPUs, numReservedCPUs)
 	if err != nil {
 		klog.Error(err)
 		return
 	}
 
-	// INFO: 由于会超卖，所以 Allocatable 不是 node 的资源实际值，可以考虑使用
-	//  machineInfo, err := cadvisorClient.MachineInfo()
-	//  capacity := cadvisor.CapacityFromMachineInfo(machineInfo), 这里 capacity 就是 currentNode.Status.Capacity
-	prodCpuRatio := float64(prodMetrics.Cpu().Value()) / float64(currentNode.Status.Allocatable.Cpu().Value())
-	nonProdCpuRatio := float64(nonProdMetrics.Cpu().Value()) / float64(currentNode.Status.Allocatable.Cpu().Value())
-	prodMemoryRatio := float64(prodMetrics.Memory().Value()) / float64(currentNode.Status.Allocatable.Memory().Value())
-	nonProdMemoryRatio := float64(nonProdMetrics.Memory().Value()) / float64(currentNode.Status.Allocatable.Memory().Value())
+	prodCPUSet := allCPUs.Difference(reserved)
+	klog.Info(fmt.Sprintf("take cpuset %s for nonProdPod, cpuset %s for ProdPod", reserved.String(), prodCPUSet.String()))
 
-	klog.Info(prodCpuRatio, nonProdCpuRatio, prodMemoryRatio, nonProdMemoryRatio)
-
+	cpuSet := allCPUs.Clone()
 	// TODO: get cpuset.CPUSet
 	for _, pod := range pods {
+		if utils.IsProdPod(pod) {
+			cpuSet = prodCPUSet.Clone()
+		} else if utils.IsNonProdPod(pod) {
+			cpuSet = reserved.Clone()
+		}
+
 		podStatus := pod.Status
 		allContainers := pod.Spec.InitContainers
 		allContainers = append(allContainers, pod.Spec.Containers...)
@@ -183,7 +259,8 @@ func (server *Server) tick(ctx context.Context, startTime time.Time) {
 				continue
 			}
 
-			cpuSet := policy.GetCPUSetOrDefault(prodCpuRatio)
+			//cpuSet := policy.GetCPUSetOrDefault(prodCpuRatio)
+
 			klog.Info(fmt.Sprintf("[cpumanager] reconcileState: updating container (pod: %s, container: %s, container id: %s, cpuset: %s)",
 				pod.Name, container.Name, containerID, cpuSet.String()))
 			err = server.updateContainerCPUSet(containerID, cpuSet)
--- go/k8s/kubelet/isolation-agent/pkg/topology/topology.go
+++
@@ -1,7 +0,0 @@
-package topology
-
-// 根据 topo 分配 cpu
-
-func GetCPUTopology() {
-
-}
