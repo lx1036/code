ec8adf1dcedd627c23090ea7ddffc6e5f0219da9
Xiang Liu <lx1036@126.com>
2021-06-09 11:34:11 +0800

    (vpa)watch kubelet evict pod generating pod oom event

--- go/k8s/monitor/vpa/recommender/pkg/input/oom/observer.go
+++ go/k8s/monitor/vpa/recommender/pkg/input/oom/observer.go
@@ -5,17 +5,18 @@ import (
 	"strings"
 	"time"
 
-	"k8s-lx1036/k8s/kubelet/pkg/eviction"
 	"k8s-lx1036/k8s/monitor/vpa/recommender/pkg/types"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
+	"k8s.io/kubernetes/pkg/kubelet/eviction"
 )
 
-// INFO: watch pod oom event
+// INFO: kubelet 在驱逐 pod 时，会产生一个 event https://github.com/kubernetes/kubernetes/blob/v1.19.7/pkg/kubelet/eviction/eviction_manager.go#L583
+// 并把 corev1.Event 对象写入 apiserver 中，这个 observer 对象会 watch 这个 Event 对象，并写入到 observedOomsChannel 中
 
 type OomInfo struct {
 	Timestamp   time.Time
@@ -43,7 +44,7 @@ func (o *Observer) OnEvent(event *corev1.Event) {
 
 func parseEvictionEvent(event *corev1.Event) []OomInfo {
 	// 必须是 Pod 产生的 Evicted 事件
-	if event.Reason != "Evicted" || event.InvolvedObject.Kind != "Pod" {
+	if event.Reason != eviction.Reason || event.InvolvedObject.Kind != "Pod" {
 		return []OomInfo{}
 	}
 
@@ -57,42 +58,87 @@ func parseEvictionEvent(event *corev1.Event) []OomInfo {
 		return strings.Split(str, ",")
 	}
 	// 三个字段值数量必须相等
+	/*
+		annotations[OffendingContainersKey] = strings.Join(containers, ",")
+		annotations[OffendingContainersUsageKey] = strings.Join(containerUsage, ",")
+		annotations[StarvedResourceKey] = string(resourceToReclaim)
+	*/
 	offendingContainers := extractArray(eviction.OffendingContainersKey)
 	offendingContainersUsage := extractArray(eviction.OffendingContainersUsageKey)
 	starvedResource := extractArray(eviction.StarvedResourceKey)
-	if len(offendingContainers) != len(offendingContainersUsage) ||
-		len(offendingContainers) != len(starvedResource) {
+	if len(offendingContainers) != len(offendingContainersUsage) || len(offendingContainers) != len(starvedResource) {
 		return []OomInfo{}
 	}
 
+	result := make([]OomInfo, 0, len(offendingContainers))
+	for i, containerName := range offendingContainers {
+		if starvedResource[i] != string(corev1.ResourceMemory) {
+			continue
+		}
+		// INFO: 这里直接通过 i 来获取 container usage，是因为 kubelet 代码里 https://github.com/kubernetes/kubernetes/blob/v1.19.7/pkg/kubelet/eviction/helpers.go#L1065-L1069
+		memory, err := resource.ParseQuantity(offendingContainersUsage[i])
+		if err != nil {
+			klog.Errorf("Cannot parse resource quantity in eviction event %v. Error: %v", offendingContainersUsage[i], err)
+			continue
+		}
+		oomInfo := OomInfo{
+			Timestamp: event.CreationTimestamp.Time.UTC(),
+			Memory:    types.ResourceAmount(memory.Value()),
+			ContainerID: types.ContainerID{
+				PodID: types.PodID{
+					Namespace: event.InvolvedObject.Namespace,
+					PodName:   event.InvolvedObject.Name,
+				},
+				ContainerName: containerName,
+			},
+		}
+		result = append(result, oomInfo)
+	}
+
+	return result
 }
+
+const (
+	evictionWatchRetryWait    = 10 * time.Second
+	evictionWatchJitterFactor = 0.5
+)
 
 // WatchEvictionEventsWithRetries INFO: 这个逻辑可以复用：watch event 事件，如果 watch 过程中有失败，则等待 jitter 时间重新去 watch
 func WatchEvictionEventsWithRetries(kubeClient kubernetes.Interface, observer Observer, namespace string, stopCh <-chan struct{}) {
-	go wait.Until(func() {
+	go func() {
 		options := metav1.ListOptions{
 			FieldSelector: "reason=Evicted",
 		}
-		watcher, err := kubeClient.CoreV1().Events(namespace).Watch(context.TODO(), options)
-		if err != nil {
-			klog.Errorf("Cannot initialize watching events. Reason %v", err)
-			return
-		}
 
-		for {
-			evictedEvent, ok := <-watcher.ResultChan()
-			if !ok {
-				klog.V(3).Infof("Eviction event chan closed")
+		watchEvictionEventsOnce := func() {
+			watcher, err := kubeClient.CoreV1().Events(namespace).Watch(context.TODO(), options)
+			if err != nil {
+				klog.Errorf("Cannot initialize watching events. Reason %v", err)
 				return
 			}
 
-			if evictedEvent.Type == watch.Added { // INFO: 只有新建的 event
-				evictedEvent, ok := evictedEvent.Object.(*corev1.Event)
+			// 读取 event 数据
+			for {
+				evictedEvent, ok := <-watcher.ResultChan() // 这里 channel 阻塞，会一直 watch 新的 event 到来
 				if !ok {
-					continue
+					klog.V(3).Infof("Eviction event chan closed")
+					return
 				}
-				observer.OnEvent(evictedEvent)
+				event, ok := evictedEvent.Object.(*corev1.Event)
+				if !ok {
+					return
+				}
+
+				observer.OnEvent(event)
 			}
 		}
-	}, time.Second*10, stopCh)
+
+		for {
+			watchEvictionEventsOnce()
+			// Wait between attempts, retrying too often breaks API server.
+			waitTime := wait.Jitter(evictionWatchRetryWait, evictionWatchJitterFactor)
+			klog.V(1).Infof("An attempt to watch eviction events finished. Waiting %v before the next one.", waitTime)
+			time.Sleep(waitTime)
+		}
+	}()
 }
