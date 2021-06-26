5336333b64604b3a0250ae4e4a15e7f9e40a77d2
Xiang Liu <lx1036@126.com>
2021-06-26 22:29:33 +0800

    move files

---
+++ go/k8s/scheduler/pkg/volcano/kube-batch/pkg/scheduler/scheduler.go
@@ -0,0 +1,78 @@
+package scheduler
+
+import (
+	"time"
+
+	"k8s-lx1036/k8s/scheduler/pkg/volcano/kube-batch/pkg/scheduler/conf"
+	"k8s-lx1036/k8s/scheduler/pkg/volcano/kube-batch/pkg/scheduler/framework"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
+)
+
+// Scheduler watches for new unscheduled pods for kubebatch. It attempts to find
+// nodes that they fit on and writes bindings back to the api server.
+type Scheduler struct {
+	cache          schedcache.Cache
+	config         *rest.Config
+	actions        []framework.Action
+	plugins        []conf.Tier
+	schedulerConf  string
+	schedulePeriod time.Duration
+}
+
+// NewScheduler returns a scheduler
+func NewScheduler(config *rest.Config, schedulerName string, conf string, period time.Duration,
+	defaultQueue string) (*Scheduler, error) {
+	scheduler := &Scheduler{
+		config:         config,
+		schedulerConf:  conf,
+		cache:          schedcache.New(config, schedulerName, defaultQueue),
+		schedulePeriod: period,
+	}
+
+	return scheduler, nil
+}
+
+// Run runs the Scheduler
+func (pc *Scheduler) Run(stopCh <-chan struct{}) {
+	var err error
+
+	// Start cache for policy.
+	go pc.cache.Run(stopCh)
+	pc.cache.WaitForCacheSync(stopCh)
+
+	// Load configuration of scheduler
+	schedConf := defaultSchedulerConf
+	if len(pc.schedulerConf) != 0 {
+		if schedConf, err = readSchedulerConf(pc.schedulerConf); err != nil {
+			klog.Errorf("Failed to read scheduler configuration '%s', using default configuration: %v",
+				pc.schedulerConf, err)
+			schedConf = defaultSchedulerConf
+		}
+	}
+
+	pc.actions, pc.plugins, err = loadSchedulerConf(schedConf)
+	if err != nil {
+		panic(err)
+	}
+
+	go wait.Until(pc.runOnce, pc.schedulePeriod, stopCh)
+}
+
+func (pc *Scheduler) runOnce() {
+	klog.V(4).Infof("Start scheduling ...")
+	scheduleStartTime := time.Now()
+	defer klog.V(4).Infof("End scheduling ...")
+	defer metrics.UpdateE2eDuration(metrics.Duration(scheduleStartTime))
+
+	ssn := framework.OpenSession(pc.cache, pc.plugins)
+	defer framework.CloseSession(ssn)
+
+	for _, action := range pc.actions {
+		actionStartTime := time.Now()
+		action.Execute(ssn)
+		metrics.UpdateActionDuration(action.Name(), metrics.Duration(actionStartTime))
+	}
+}
---
+++ go/k8s/scheduler/pkg/volcano/metrics/metrics.md
@@ -0,0 +1,28 @@
+
+## Volcano Metrics
+
+```yaml
+volcano_e2e_scheduling_latency_milliseconds: (histogram) volcano scheduler 调度插件调度pod和binding pod的整个过程耗费时间
+
+volcano_e2e_job_scheduling_duration: (gauge)
+  avg(volcano_e2e_job_scheduling_duration{}) by (queue): (按queue分组)过去24小时内调度属于该queue的pods平均耗时
+  avg(volcano_e2e_job_scheduling_duration{}) by (job_namespace): (按namespace分组)过去24小时内调度属于该queue的pods平均耗时
+  increase(volcano_e2e_job_scheduling_duration{}[24h]) != 0: 过去24小时内volcano scheduler调度pods耗时的增量
+  stddev(volcano_e2e_job_scheduling_duration)/avg(volcano_e2e_job_scheduling_duration): 标准差除以平均数，为变异系数CV(Coefficient of Variation) https://baike.baidu.com/item/%E5%8F%98%E5%BC%82%E7%B3%BB%E6%95%B0
+
+
+kube_pod_volcano_container_resource_requests: (gauge)
+  sum(kube_pod_volcano_container_resource_requests{resource="memory", unit="byte",job="kube-state-metrics",queue!=""}) by (queue): (按queue分组)该queue下所有pod使用memory总和
+  sum(kube_pod_volcano_container_resource_requests{resource="memory", unit="byte",job="kube-state-metrics"}) by (volcano_namespace): (按namespace分组)该queue下所有pod使用memory总和
+
+
+
+缺少的metrics:
+  plugins:
+    plugin_scheduling_latency_microseconds: grafana dashboard 缺少每一个 plugin 的耗时
+
+
+```
+
+
+
