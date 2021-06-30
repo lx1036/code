6e1d04e31d01aab87c56af21c8275e649773ee7c
Xiang Liu <lx1036@126.com>
2021-06-30 20:33:14 +0800

    (log)filebeat

--- go/k8s/storage/log/filebeat.sh
+++
@@ -1,4 +0,0 @@
-
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.6.1-darwin-x86_64.tar.gz
-tar xzvf filebeat-7.6.1-darwin-x86_64.tar.gz
-cd filebeat-7.6.1-darwin-x86_64/
--- go/k8s/storage/log/kibana.sh
+++
@@ -1,4 +0,0 @@
-curl -O https://artifacts.elastic.co/downloads/kibana/kibana-7.6.1-darwin-x86_64.tar.gz
-curl https://artifacts.elastic.co/downloads/kibana/kibana-7.6.1-darwin-x86_64.tar.gz.sha512 | shasum -a 512 -c -
-tar -xzf kibana-7.6.1-darwin-x86_64.tar.gz
-cd kibana-7.6.1-darwin-x86_64/
