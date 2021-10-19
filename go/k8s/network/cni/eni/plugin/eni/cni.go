08649cf168fdeeb3a8d81fb5243e2223b2194437
Xiang Liu <lx1036@126.com>
2021-10-19 19:18:19 +0800

    typo

--- go/k8s/network/cni/eni/plugin/eni/cni.go
+++ go/k8s/network/cni/eni/plugin/eni/cni.go
@@ -77,6 +77,9 @@ type K8SArgs struct {
 }
 
 func init() {
+	// This ensures that main runs only on main thread (thread group leader).
+	// since namespace ops (unshare, setns) are done for a single thread, we
+	// must ensure that the goroutine does not jump from OS thread to thread
 	runtime.LockOSThread()
 }
 
