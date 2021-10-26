7f1fb32059d1a1fca49801d043064fc26fc9a268
Xiang Liu <lx1036@126.com>
2021-10-26 19:06:05 +0800

    (cni)eni for vm

---
+++ go/k8s/network/cni/eni/pkg/link/interface.go
@@ -0,0 +1,27 @@
+package link
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/vishvananda/netlink"
+)
+
+// GetDeviceNumber get interface device number by mac address
+func GetDeviceNumber(mac string) (int32, error) {
+	linkList, err := netlink.LinkList()
+	if err != nil {
+		return 0, errors.Wrapf(err, "error get link list from netlink")
+	}
+
+	for _, link := range linkList {
+		// ignore virtual nic type. eg. ipvlan veth bridge
+		if _, ok := link.(*netlink.Device); !ok {
+			continue
+		}
+		if link.Attrs().HardwareAddr.String() == mac {
+			return int32(link.Attrs().Index), nil
+		}
+	}
+
+	return 0, fmt.Errorf("can't found dev by mac %s", mac)
+}
---
+++ go/k8s/network/cni/eni/pkg/utils/ip.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+)
+
+// ToIP parse str to net.IP and return error is parse failed
+func ToIP(addr string) (net.IP, error) {
+	ip := net.ParseIP(addr)
+	if ip == nil {
+		return nil, fmt.Errorf("failed to parse ip %s", addr)
+	}
+	return ip, nil
+}
