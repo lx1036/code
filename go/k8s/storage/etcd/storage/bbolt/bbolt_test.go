8c6be7186a19d2be01f0eaf0958b7aba29575218
Xiang Liu <lx1036@126.com>
2021-12-23 18:57:13 +0800

    (csi)boltdb demo

--- go/k8s/storage/etcd/storage/bbolt/bbolt_test.go
+++ go/k8s/storage/etcd/storage/bbolt/bbolt_test.go
@@ -127,16 +127,43 @@ func TestBbolt(test *testing.T) {
 }
 
 func TestBuckets(test *testing.T) {
-	db, err := bolt.Open("/Users/lx1036/Code/lx1036/code/go/k8s/storage/etcd/cluster1/etcd1/member/snap/db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	db, err := bolt.Open("/Users/liuxiang/Code/lx1036/code/go/k8s/storage/etcd/cluster1/etcd1/member/snap/db", 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		klog.Fatal(err)
 	}
 	defer db.Close()
 
-	db.View(func(tx *bolt.Tx) error {
+	/*db.View(func(tx *bolt.Tx) error { // all buckets
 		return tx.ForEach(func(name []byte, b *bolt.Bucket) error {
 			klog.Info(string(name))
 			return nil
 		})
 	})
+	
+	db.View(func(tx *bolt.Tx) error {
+		return tx.Bucket([]byte("key")).ForEach(func(k, v []byte) error {
+			//klog.Info(fmt.Sprintf("key:%s value:%s", k, v))
+			return nil
+		})
+	})*/
+	
+	tx, _ := db.Begin(false)
+	defer func() {
+		tx.Rollback()
+	}()
+	c := tx.Bucket([]byte("key")).Cursor()
+	key, value := c.First()
+	klog.Info(fmt.Sprintf("value:%s", value))
+	for i := 0; i < 100; i++ {
+		key, value = c.Next()
+		if key == nil {
+			break
+		}
+		klog.Info(fmt.Sprintf("key:%s value:%s", key, value))
+	}
+	/*for key, value := c.Next(); key != nil; {
+		klog.Info(fmt.Sprintf("key:%s value:%s", key, value))
+	}*/
+	
+	klog.Info("finish")
 }
