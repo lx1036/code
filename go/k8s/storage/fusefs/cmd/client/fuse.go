a412f47d8c3fa56ecfc314e406741c23a554f333
Xiang Liu <lx1036@126.com>
2022-01-05 17:39:17 +0800

    typo

--- go/k8s/storage/fusefs/cmd/client/fuse.go
+++ go/k8s/storage/fusefs/cmd/client/fuse.go
@@ -15,6 +15,19 @@ import (
 	"k8s.io/klog/v2"
 )
 
+// INFO: VFS(虚拟文件系统) = 超级块super_block + inode + dentry + vfsmount
+//  inode 索引节点: 记录文件系统对象中的一般元数据，比如 kind/size/created/modified/sharing&permissions/指向存储该内容的磁盘区块的指针/文件分类
+//  dentry 目录项: 记录文件系统对象在全局文件系统树中的位置，例如：open一个文件/home/xxx/yyy.txt，那么/、home、xxx、yyy.txt都是一个目录项，
+//  VFS在查找的时候，根据一层一层的目录项找到对应的每个目录项的inode，那么沿着目录项进行操作就可以找到最终的文件。
+//  **[Linux 虚拟文件系统四大对象：超级块、inode、dentry、file之间关系](https://www.eet-china.com/mp/a38145.html)**
+
+// INFO: VFS 就是一层接口，定义了一些接口函数
+//  VFS是一种软件机制，只存在于内存中，每次系统初始化期间Linux都会先在内存中构造一棵VFS的目录树（也就是源码中的namespace）。
+//  VFS主要的作用是对上层应用屏蔽底层不同的调用方法，提供一套统一的调用接口，二是便于对不同的文件系统进行组织管理。
+//  VFS提供了一个抽象层，将POSIX API接口与不同存储设备的具体接口实现进行了分离，使得底层的文件系统类型、设备类型对上层应用程序透明。
+//  例如read，write，那么映射到VFS中就是sys_read，sys_write，那么VFS可以根据你操作的是哪个“实际文件系统”(哪个分区)来进行不同的实际的操作！这个技术也是很熟悉的“钩子结构”技术来处理的。
+//  其实就是VFS中提供一个抽象的struct结构体，然后对于每一个具体的文件系统要把自己的字段和函数填充进去，这样就解决了异构问题（内核很多子系统都大量使用了这种机制）
+
 const (
 	DefaultPageSize        = 1 << 17 // 128KB
 	DefaultBlksize         = 1 << 20 // 1M
--- go/k8s/storage/fusefs/pkg/raftstore/store_memory.go
+++
@@ -1,9 +0,0 @@
-package raftstore
-
-// INFO: memory 作为后端 meta 数据后端存储引擎，后续改成 boltDB
-type MemoryStore struct {
-}
-
-func NewMemoryStore() *MemoryStore {
-	return &MemoryStore{}
-}
