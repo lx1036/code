84b65f511e0fc40d712c3816d1406413d1447af8
Xiang Liu <lx1036@126.com>
2022-07-06 19:53:45 +0800

    typo

--- go/k8s/network/cilium/cilium/bpf/lib/policy.h
+++ go/k8s/network/cilium/cilium/bpf/lib/policy.h
@@ -11,6 +11,8 @@ static __always_inline int
 policy_sk_egress(__u32 identity, __u32 ip,  __u16 dport)
 {
     void *map = lookup_ip4_endpoint_policy_map(ip);
+    if (!map)
+        return CTX_ACT_OK;
 }
 
 
