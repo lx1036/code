adb5858f906a8298f3cbb7aae7f474724b52d487
Xiang Liu <lx1036@126.com>
2022-09-06 18:14:18 +0800

    algo
    Signed-off-by: Xiang Liu <lx1036@126.com>

--- go/k8s/algo/leetcode/sliding-window/003_longest_substring_test.go
+++ go/k8s/algo/leetcode/sliding-window/003_longest_substring_test.go
@@ -34,6 +34,55 @@ func lengthOfLongestSubstring(s string) int {
 	return result
 }
 
+// 无重复最长子串用这个函数，滑动窗口方法，两个指针解决
+func lengthOfLongestSubstring2(s string) int {
+	result := 0
+	storage := make(map[byte]int)
+	l := len(s)
+	for i := 0; i < l; i++ {
+		tmp := 0
+		for j := i; j < l; j++ {
+			if _, ok := storage[s[j]]; !ok {
+				storage[s[j]] = j
+				tmp++
+			} else {
+				break
+			}
+		}
+
+		if tmp > result {
+			result = tmp
+		}
+
+		storage = map[byte]int{}
+	}
+
+	return result
+}
+
+func TestLengthOfLongestSubstring2(test *testing.T) {
+	hashTable := map[byte]int{}
+	fmt.Println(hashTable['a']) // 0
+
+	s := "abcabcbb"
+	assert.Equal(test, 3, lengthOfLongestSubstring2(s))
+
+	s = "bbbbb"
+	assert.Equal(test, 1, lengthOfLongestSubstring2(s))
+
+	s = "pwwkew"
+	assert.Equal(test, 3, lengthOfLongestSubstring2(s))
+
+	s = "ab"
+	assert.Equal(test, 2, lengthOfLongestSubstring2(s))
+
+	s = " "
+	assert.Equal(test, 1, lengthOfLongestSubstring2(s))
+
+	s = "dvdf"
+	assert.Equal(test, 3, lengthOfLongestSubstring2(s))
+}
+
 func TestLengthOfLongestSubstring(test *testing.T) {
 	hashTable := map[byte]int{}
 	fmt.Println(hashTable['a']) // 0
