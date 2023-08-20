54d3d39197a0c5c64e6ed83b636c93f3aeece1bb
shenming <shenming.lx@alibaba-inc.com>
2023-08-20 13:22:58 +0800

    add golang for TCP FastOpen

--- go/k8s/bpf/tubular/toa/internal/tcp/tcp.go
+++ go/k8s/bpf/tubular/toa/internal/tcp/tcp.go
@@ -7,7 +7,7 @@ import (
 	"net"
 )
 
-func startServer() int {
+func startServer(network, serverAddr string) int {
 	var sk int
 	var err error
 	defer func() {
@@ -20,8 +20,12 @@ func startServer() int {
 	if err != nil {
 		return -1
 	}
+	err = unix.SetsockoptInt(sk, unix.SOL_TCP, unix.TCP_FASTOPEN, 256)
+	if err != nil {
+		return -1
+	}
 
-	laddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
+	laddr, err := net.ResolveTCPAddr(network, serverAddr)
 	// laddr, err := net.ResolveTCPAddr("tcp", "[::]:8080")
 	if err != nil {
 		logrus.Errorf("Error resolving local address: %v", err)
@@ -48,59 +52,8 @@ func startServer() int {
 	return sk
 }
 
-func connectToFd() int {
-	var csk int
-	var err error
-	defer func() {
-		if err != nil {
-			logrus.Error(err)
-			unix.Close(csk)
-		}
-	}()
-
-	csk, err = unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
-	if err != nil {
-		return -1
-	}
-
-	raddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
-	if err != nil {
-		logrus.Errorf("Error resolving local address: %v", err)
-		return -1
-	}
-	rsockaddr := &unix.SockaddrInet4{
-		Port: raddr.Port,
-		Addr: [4]byte{},
-	}
-	copy(rsockaddr.Addr[:], raddr.IP.To4())
-	err = unix.Connect(csk, rsockaddr)
-
-	return csk
-}
-
-func fastOpenConnect() int {
-	var csk int
-	var err error
-	defer func() {
-		if err != nil {
-			logrus.Errorf("connect %v", err)
-			unix.Close(csk)
-		}
-	}()
-
-	csk, err = unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
-	if err != nil {
-		return -1
-	}
-
-	data := []byte("FAST!!!")
-	unix.Sendto(csk, data, unix.MSG_FASTOPEN, &unix.SockaddrInet4{Port: 8080})
-
-	return csk
-}
-
-func startSocketServer() {
-	sk := startServer()
+func startSocketServer(network, serverAddr string) {
+	sk := startServer(network, serverAddr)
 
 	for {
 		clientSocket, clientAddr, err := unix.Accept(sk)
@@ -115,23 +68,93 @@ func startSocketServer() {
 	}
 }
 
+func handleConnection(clientSocket int) {
+	data := make([]byte, 1024)
+	// INFO: Recvfrom() / Read() 都可以, 因为已经 establish
+	// n, _, err := unix.Recvfrom(clientSocket, data, 0)
+	n, err := unix.Read(clientSocket, data)
+	if err != nil {
+		log.Printf("read failed: %v\n", err)
+		return
+	}
+	logrus.Infof("read %d number bytes from client: %s", n, string(data[:n]))
+
+	data = []byte("hello client!")
+	n, err = unix.Write(clientSocket, data)
+	if err != nil {
+		log.Printf("write failed: %v\n", err)
+		return
+	}
+	logrus.Infof("write %d number bytes to client", n)
+}
+
+func connectToFd(network, serverAddr string) int {
+	var csk int
+	var err error
+	defer func() {
+		if err != nil {
+			logrus.Error(err)
+			unix.Close(csk)
+		}
+	}()
+
+	csk, err = unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
+	if err != nil {
+		return -1
+	}
+
+	raddr, err := net.ResolveTCPAddr(network, serverAddr)
+	if err != nil {
+		logrus.Errorf("Error resolving local address: %v", err)
+		return -1
+	}
+	rsockaddr := &unix.SockaddrInet4{
+		Port: raddr.Port,
+		Addr: [4]byte{},
+	}
+	copy(rsockaddr.Addr[:], raddr.IP.To4())
+	err = unix.Connect(csk, rsockaddr)
+
+	return csk
+}
+
+func fastOpenConnect(network, serverAddr string) int {
+	var csk int
+	var err error
+	defer func() {
+		if err != nil {
+			logrus.Errorf("connect %v", err)
+			unix.Close(csk)
+		}
+	}()
+
+	csk, err = unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
+	if err != nil {
+		return -1
+	}
+	err = unix.SetsockoptInt(csk, unix.SOL_TCP, unix.TCP_FASTOPEN, 256)
+	if err != nil {
+		return -1
+	}
+
+	raddr, err := net.ResolveTCPAddr(network, serverAddr)
+	rsockAddr := &unix.SockaddrInet4{
+		Port: raddr.Port,
+		Addr: [4]byte{},
+	}
+	copy(rsockAddr.Addr[:], raddr.IP.To4())
+	data := []byte("hello server!")
+	unix.Sendto(csk, data, unix.MSG_FASTOPEN, rsockAddr)
+	// INFO: Write() 不行，使用 Write() 必须要先 Connect()
+	//unix.Write(csk, data)
+
+	return csk
+}
+
 func getAddr(addr *unix.SockaddrInet4) (string, int) {
 	cAddr := addr.Addr
 	cPort := addr.Port
 	ipv4 := net.IPv4(cAddr[0], cAddr[1], cAddr[2], cAddr[3])
 
 	return ipv4.String(), cPort
-}
-
-func handleConnection(clientSocket int) {
-	var data []byte
-	n, err := unix.Read(clientSocket, data)
-	if err != nil {
-		log.Printf("read failed: %v\n", err)
-		return
-	}
-
-	logrus.Infof("read %d number bytes from client", n)
-
-	// unix.Write(clientSocket, data[:n])
 }
--- go/k8s/bpf/tubular/toa/internal/tcp/tcp_test.go
+++ go/k8s/bpf/tubular/toa/internal/tcp/tcp_test.go
@@ -10,12 +10,11 @@ import (
 // 使用 go 来测试 tcp fastopen，但是必须在 linux 环境里才可以，本地 mac 不行。
 // 目前还没有调试成功
 
-
 // go test -v -run ^TestFastOpen$ .
 func TestFastOpen(test *testing.T) {
-	stopCh := time.After(time.Second * 5)
+	expireCh := time.After(time.Second * 5)
 
-	go startSocketServer()
+	go startSocketServer("tcp", ":6080") // nginx: listen 6080 fastopen=256;
 
 	tick := time.NewTicker(time.Second)
 	defer tick.Stop()
@@ -23,14 +22,25 @@ func TestFastOpen(test *testing.T) {
 		select {
 		case <-tick.C:
 			clientSendto()
-		case <-stopCh:
+		case <-expireCh:
 			return
 		}
 	}
 }
 
 func clientSendto() {
-	clientSk := fastOpenConnect()
+	clientSk := fastOpenConnect("tcp", ":6080")
+	defer unix.Close(clientSk)
+
+	data := make([]byte, 1024)
+	//unix.Read()
+	n, _, err := unix.Recvfrom(clientSk, data, 0)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+	logrus.Infof("recv %d number bytes from server: %s", n, string(data[:n]))
+
 	clientAddr, err := unix.Getsockname(clientSk)
 	if err != nil {
 		logrus.Error(err)
@@ -46,29 +56,4 @@ func clientSendto() {
 	}
 	sAddrStr, sPort := getAddr(serverAddr.(*unix.SockaddrInet4))
 	logrus.Infof("server addr %s:%d", sAddrStr, sPort)
-
 }
-
-//func fastOpenConnect() int {
-//	sk, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, 0)
-//	if err != nil {
-//		logrus.Errorf("%+v", err)
-//		return sk
-//	}
-//	var optVal = uint32(1)
-//	err = unix.SetsockoptInt(sk, unix.IPPROTO_TCP, unix.TCP_FASTOPEN, int(optVal))
-//	if err != nil {
-//		logrus.Errorf("%+v", err)
-//		return sk
-//	}
-//	var addr unix.SockaddrInet4
-//	addr.Family = unix.AF_INET
-//	addr.Port = 9000
-//	addr.Addr[0] = byte(172)
-//	addr.Addr[1] = byte(16)
-//	addr.Addr[2] = byte(1)
-//	addr.Addr[3] = byte(1)
-//	err = unix.Connect(sk, &addr)
-//	if err == unix.EINPROGRESS || err == unix.EAL
-//
-//}
